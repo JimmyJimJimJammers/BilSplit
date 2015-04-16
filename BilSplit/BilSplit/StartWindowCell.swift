3e5fdb97fc4348a8e800ac3c394d442aa8589275
JimmyJimJimJammers <mccarthyj10@aol.com>
2015-04-16 00:48:01 -0400

    -Added some stuff.

---
+++ BilSplit/BilSplit/StartWindowCell.swift
@@ -0,0 +1,30 @@
+//
+//  StartWindowCell.swift
+//  BilSplit
+//
+//  Created by James McCarthy on 4/16/15.
+//  Copyright (c) 2015 PandaTech. All rights reserved.
+//
+
+import UIKit
+
+class StartWindowCell: UITableViewCell
+{
+
+    @IBOutlet weak var LocationLabel: UILabel!
+    @IBOutlet weak var TotalLabel: UILabel!
+    @IBOutlet weak var DateLabel: UILabel!
+    
+    override func awakeFromNib()
+    {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool)
+    {
+        super.setSelected(selected, animated: animated)
+    }
+    
+
+}
